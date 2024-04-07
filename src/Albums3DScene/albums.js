ee61afa1a203ca619d1608dfab38e4b0f680c9b3
Dea Bankova <d.y.bankova@gmail.com>
2024-04-07 10:40:12 +0100

    test

--- src/Albums3DScene/albums.js
+++ src/Albums3DScene/albums.js
@@ -14,6 +14,22 @@ const albums = [
   { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], imgUrl: './1V6a99EbTTIegOhWoPxYI9.png', id: '1V6a99EbTTIegOhWoPxYI9', name: 'Arrival', colorDark: '#481810', colorLight: '#D2462F' }, 
   { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0],  imgUrl: './5gSBDA6ufk8UZejT4XR7av.png', id: '5gSBDA6ufk8UZejT4XR7av', name: 'Waterloo', colorDark: '#903828', colorLight: '#CB4F38'  } 
 ]
+
+const albumsTest = [
+  // Front
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], imgUrl: pexel(1103970), id: '0uUtGVj0y9FjfKful7cABY', name: 'Voyage', colorDark: '#93711C', colorLight: '#D0A028' }, 
+  // Back
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0],  imgUrl: pexel(416430), id: '3Il2ad1NGO9zoY1FIVBuHD', name: 'Ring Ring', colorDark: '#782828', colorLight: '#D14646' }, 
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], imgUrl: pexel(310452), id: '3JHZ83ohEBDLFQfbVfDHFh', name: 'The Visitors', colorDark: '#BB5D10', colorLight: '#C06010' }, 
+  // Left
+  { position: [-1.65, 0, 0.25], rotation: [0, Math.PI / 3, 0], imgUrl:  pexel(327482), id: '5fLOHW1UXr1cJrnXiU3FBt', name: 'The Album', colorDark: '#C4506D', colorLight: '#D85878' }, 
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], imgUrl: pexel(325185), id: '3ZdkT5buYFi1WQaB0XNNtf', name: 'Super Trouper', colorDark: '#481010', colorLight: '#E13232' }, 
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], imgUrl: pexel(358574), id: '7iLuHJkrb9KHPkMgddYigh', name: 'Voulez-Vous', colorDark: '#0068A0', colorLight: '#007CBF' }, 
+  // Right
+  { position: [1.65, 0, 0.25], rotation: [0, -Math.PI / 3, 0], imgUrl: pexel(227675), id: '1kM6xcSYO5ASJaWgygznL7', name: 'ABBA', colorDark: '#482020', colorLight: '#C05555' }, 
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], imgUrl: pexel(911738), id: '1V6a99EbTTIegOhWoPxYI9', name: 'Arrival', colorDark: '#481810', colorLight: '#D2462F' }, 
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0],  imgUrl: pexel(1738986), id: '5gSBDA6ufk8UZejT4XR7av', name: 'Waterloo', colorDark: '#903828', colorLight: '#CB4F38'  } 
+]
 // const images = [
 //   // Front
 //   { position: [0, 0, 1.5], rotation: [0, 0, 0], imgUrl: pexel(1103970), id: '1103970' },
@@ -30,4 +46,4 @@ const albums = [
 //   { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], imgUrl: pexel(1738986), id: '1738986' }
 // ]
 
-export default albums;
+export default albumsTest;
