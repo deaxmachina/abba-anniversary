0e3fc71bd64c76328dfb736f0eeb07bfdbd22df5
Dea Bankova <d.y.bankova@gmail.com>
2024-04-07 14:45:27 +0100

    testing the show constraits

--- src/NotWorkingScreen/NotWorkingScreen.jsx
+++ src/NotWorkingScreen/NotWorkingScreen.jsx
@@ -3,9 +3,9 @@ import './NotWorkingScreen.scss'
 const NotWorkingScreen = () => {
 
   return (
-    <>
+    <div className='wrapper-not-working'>
       Not working yo
-    </>
+    </div>
   )
 }
 
