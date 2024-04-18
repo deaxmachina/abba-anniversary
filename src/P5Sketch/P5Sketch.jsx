b5b3be82aeecd3624a6a751bb390258866ee5a56
Dea Bankova <d.y.bankova@gmail.com>
2024-04-18 20:56:58 +0100

    inermediate working 1

--- src/P5Sketch/P5Sketch.jsx
+++ src/P5Sketch/P5Sketch.jsx
@@ -1,4 +1,5 @@
 import './P5Sketch.scss'
+import { selectedSongs } from './selectedSongs.js'
 import audioFeatures from '../data/audio_features_results.json'
 import albums from '../data/albums.json'
 import audioPreviews from '../data/songs_coverArt_and_audioPreviews.json'
@@ -8,6 +9,8 @@ import * as d3 from 'd3'
 import p5 from 'p5'
 window.p5 = p5
 import("p5/lib/addons/p5.sound")
+
+const selectedAlbumId = '1kM6xcSYO5ASJaWgygznL7' // '0uUtGVj0y9FjfKful7cABY'
 
 const glowMetricMoons = '#fcedbb' // '#fff'
 const glowSongMoon = '#fcedbb' // '#fff'
@@ -24,20 +27,30 @@ const P5Sketch = () => {
   const canvasRef = useRef()
   const songRef = useRef()
 
+  // Get tracks in current album 
+  const tracksInAlbum = albums.find(d => d.id === selectedAlbumId).tracks
+
   useEffect(() => {
-    let song
     let fft
+    let songId
+    let audioFeaturesSong
     const fftBins = 64
     const fftBinCutoff = 41
-    let texture
+
+    let buttonPlayPause
+    let buttonExitSongs
+    
+    let graphData = null
+    let links = null
+    let nodes = null
+    let simulation = null
+    let nodesSimulation = null
+    let linksSimulation = null
 
     const sketch = new p5((p) => {
-      // console.log('loadSound', p5Sound)
 
       p.preload = () => {
-        song = p.loadSound(audioUrl)
         songRef.current = p.loadSound(audioUrl)
-        texture = p.loadImage('texture.jpg');
       }
 
       p.setup = () => {
@@ -49,6 +62,83 @@ const P5Sketch = () => {
         // Instantial the fft
 	      fft = new p.constructor.FFT(0.8, fftBins)
 
+
+      	////////////////////////////////////////////
+        ///////// Nodes and Links creation /////////
+        ////////////////////////////////////////////
+        // Create data for node-link graph 
+        // Make sure that every song is connected to at least one more song
+        const compulsoryLinks = tracksInAlbum.map(track => {
+          const source = track.id
+          const target = _.sample(tracksInAlbum.filter(d => d.id !== source)).id
+          return ({ source, target, size: _.sample([0, 0.7, 1.6]) })
+        })
+        // Thow in some more random links for funsies
+        const otherLinks = _.range(6).map(i => {
+          const source = _.sample(tracksInAlbum).id
+          const target = _.sample(tracksInAlbum.filter(d => d.id !== source)).id
+          return ({ source, target, size: _.sample([0, 0.6, 0.5, 0.7, 1.8, 4])  })
+        })
+        const allLinks = _.uniqBy([...compulsoryLinks, ...otherLinks], d => `${d.source}-${d.target}`)
+        const data = { 
+          nodes: tracksInAlbum.map(track => ({
+              ...track, 
+              size:  _.find(selectedSongs, d => d.albumId === selectedAlbumId).songs.map(songs => songs.id).includes(track.id) 
+                ? 60
+                :  _.sample([25, 30, 40]),
+              col: colRays,
+          })), 
+          links: allLinks
+        }
+        graphData = data
+        links = graphData.links.map(d => ({...d})) // Need a copy of links as the force will modify the array in place
+        nodes = graphData.nodes.map(d => ({...d})) // Need a copy of nodes as the force will modify the array in place
+
+
+        ////////////////////////////////////////////
+        ///////////// Force simulation /////////////
+        ////////////////////////////////////////////
+        simulation = d3.forceSimulation(nodes)
+          .force("link", d3.forceLink(links).id(d => d.id))
+          .force("charge", d3.forceManyBody())
+          .force("center", d3.forceCenter(width * 0.5, height * 0.5).strength(1.2))
+          .force("x", d3.forceX((d, i) => i * 50).strength(0.5))
+          .force("collide", d3.forceCollide().radius(50).strength(0.2))
+          .on("tick", () => {
+            nodesSimulation = [...nodes]
+            linksSimulation = [...links]
+          })
+
+
+        ///////////////////////////////////////////
+        ///////////// Button controls /////////////
+        ///////////////////////////////////////////
+        // Play / pause button
+        buttonPlayPause = p.select('.toggle-btn')
+        buttonPlayPause.html('pa')
+        buttonPlayPause.mousePressed(() => {
+          if (songRef.current) {
+            if (songRef.current.isPlaying()) {
+              songRef.current.pause()
+              buttonPlayPause.html('pl')
+            } else {
+              songRef.current.play()
+              buttonPlayPause.html('pa')
+            }
+          }
+        })
+
+        // Button to set song to null 
+        buttonExitSongs = p.select('.exit-songs-btn')
+        // buttonExitSongs.addClass('hide')
+        // buttonExitSongs.removeClass('show')
+        buttonExitSongs.mousePressed(() => {
+          if (songRef.current) songRef.current.pause()
+          songRef.current = songUrl = songId = audioFeaturesSong = clickedNodeId = songName = null
+          // Hide the play / pause button 
+          // buttonPlayPause.removeClass('show')
+          // buttonPlayPause.addClass('hide')
+        })
 
         p.background(220)
 
@@ -87,7 +177,7 @@ const P5Sketch = () => {
       style={{ width: `${width}px`, height: `${height}px` }}
     >
       <div ref={canvasRef} className='wrapper-sketch' style={{ width: `${width}px`, height: `${height}px`, position: 'relative' }}>
-        <button
+        {/* <button
           style={{ position: 'absolute', top: '0px', left: '0px' }}
           onClick={() => {
             if (songRef.current) {
@@ -98,7 +188,11 @@ const P5Sketch = () => {
               }
             }
           }}
-        >soo0000ong</button>
+        >soo0000ong</button> */}
+
+        <button class='toggle-btn'>pl</button>
+        <button class='exit-songs-btn'>bk</button>
+
         <div className='sketch-overlay'></div>
       </div>
     </div>
---
+++ src/P5Sketch/selectedSongs.js
@@ -0,0 +1,85 @@
+export const selectedSongs = [
+  {
+    albumId: "7iLuHJkrb9KHPkMgddYigh",
+    albumName: "Voulez-Vous",
+    songs: [
+      { id: '17OqI90oTFZ3J8PVu6j07V', name: 'Voulez-Vous' },
+      { id: '1PtJclc46wTk367PlsU6Uj', name: 'I Have A Dream' },
+      { id: '762B4bOcXF7I2Y8UlKTyTy', name: 'Chiquitita' },
+      { id: '3vkQ5DAB1qQMYO4Mr9zJN6', name: 'Gimme! Gimme! Gimme! (A Man After Midnight)' },
+      { id: '', name: '' },
+    ]
+  },
+  {
+    albumId: "3ZdkT5buYFi1WQaB0XNNtf",
+    albumName: "Super Trouper",
+    songs: [
+      { id: '0J2p4KYdr6Mg4ET6JPlbe1', name: 'Super Trouper' },
+      { id: '3oEkrIfXfSh9zGnE7eBzSV', name: 'The Winner Takes It All' },
+      { id: '4AsvGVDWs16fqIiIdDzyvX', name: 'Happy New Year' },
+      { id: '4euAGZTszWPrriggYK0HG9', name: 'Lay All Your Love On Me' },
+    ]
+  },
+  {
+    albumId: "5fLOHW1UXr1cJrnXiU3FBt",
+    albumName: "The Album",
+    songs: [
+      { id: '5BckPAYcKEJuYs1eV1BHHe', name: 'Take A Chance On Me' },
+      { id: '08GOw3NsrJ0LsCCeyqzt3b', name: 'Thank You For The Music' },
+    ]
+  },
+  {
+    albumId: "3Il2ad1NGO9zoY1FIVBuHD",
+    albumName: "Ring Ring",
+    songs: [
+      { id: '5anrNIgDuSFsP2DJgAIRxU', name: 'Ring Ring - English Version' },
+    ]
+  },  
+  {
+    albumId: "3JHZ83ohEBDLFQfbVfDHFh",
+    albumName: "The Visitors",
+    songs: [
+      { id: '6tlF4uh34QXqq81XrIyO0O', name: 'When All Is Said And Done' },
+      { id: '1lJnJEccctzI7FvwvtAt8r', name: 'One Of Us' },
+      { id: '4OkSYRRFb3UMXtTj1SnBOR', name: 'Slipping Through My Fingers' },
+    ]
+  },  
+  {
+    albumId: "1kM6xcSYO5ASJaWgygznL7",
+    albumName: "Abba",
+    songs: [
+      { id: '2TxCwUlqaOH3TIyJqGgR91', name: 'Mamma Mia' },
+      { id: '6yZv0Nl6BXABbXoPVpfF5y', name: 'SOS' },
+      { id: '0x5BKBAcia1th7UzcoHnD1', name: 'I Do, I Do, I Do, I Do, I Do' },
+    ]
+  },
+  {
+    albumId: "1V6a99EbTTIegOhWoPxYI9",
+    albumName: "Arrival",
+    songs: [
+      { id: '0GjEhVFGZW8afUYGChu3Rr', name: 'Dancing Queen' },
+      { id: '798cuJeotvXP8UVa8GJPnD', name: 'Knowing Me, Knowing You' },
+      { id: '3lKN03PDeBnXZS7sDukRBt', name: 'Money, Money, Money' },
+      { id: '4BM8yJ0PzBi2ZewpMTOxtx', name: 'Fernando' },
+    ]
+  },
+  {
+    albumId: "5gSBDA6ufk8UZejT4XR7av",
+    albumName: "Waterloo",
+    songs: [
+      { id: '3Dy4REq8O09IlgiwuHQ3sk', name: 'Waterloo' },
+    ]
+  },
+  {
+    albumId: "0uUtGVj0y9FjfKful7cABY",
+    albumName: "Voyage",
+    songs: [
+      { id: '3D9TYPbNo1nnTwE4eLKxnJ', name: 'I Still Have Faith In You' },
+      { id: '3ARUWZ4hryYXhp6X7KHcaD', name: "Don't Shut Me Down" },
+      { id: '', name: "" },
+      { id: '', name: '' },
+      { id: '', name: "" },
+      { id: '', name: '' },
+    ]
+  }
+]
