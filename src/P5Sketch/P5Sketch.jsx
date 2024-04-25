390998123a3e996d2f024233f06b6e87cda50244
Dea Bankova <d.y.bankova@gmail.com>
2024-04-25 19:19:30 +0100

    test

--- src/P5Sketch/P5Sketch.jsx
+++ src/P5Sketch/P5Sketch.jsx
@@ -334,12 +334,13 @@ const P5Sketch = ({ windowWidth, windowHeight, selectedAlbumId, colours, mum, fu
 
         // Only in the mode where we want to limit the song duration !!!
         // Stop and reset the song after 15 seconds
-        console.log(songRef.current.currentTime())
+        //console.log(songRef.current.currentTime())
         if (songRef.current.currentTime() > 5) {
-          console.log(songRef.current.currentTime())
-          songRef.current.jump()
+          songRef.current.jump(0)
           songRef.current.pause()
         }
+
+        
 
 
         p.background(0)
